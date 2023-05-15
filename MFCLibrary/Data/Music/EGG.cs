5df8873043a5b5cc910e988e092d6c85b864e24d
VajN <131675285+VajN@users.noreply.github.com>
2023-05-15 21:19:05 +0300

    Пасхалка

---
+++ MFCLibrary/Data/Music/EGG.cs
@@ -0,0 +1,13 @@
+﻿
+namespace MFCLibrary.Data.Music
+{
+    public static class EGG
+    {
+        public static void SovietUnion()
+        {
+            System.Media.SoundPlayer SU = new(@"EGG.wav");
+            SU.Play();
+            Console.Read();
+        }
+    }
+}
